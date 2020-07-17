79b04daea49d8c89573f76c8e2acdbfccc6a17cf
dteethman <zuuuubice@gmail.com>
2020-07-17 15:13:01 +0300

    Add Colors extension

---
+++ nekurumba/Extensions/Colors.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+let bgLightColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+let bgDarkColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+let bgColors = (light: bgLightColor, dark: bgDarkColor)
