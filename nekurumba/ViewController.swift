2cef0e9ebf6c7d5d1d055d28a9773232ae01c17a
dteethman <zuuuubice@gmail.com>
2020-07-17 08:39:31 +0300

    Initial Commit

---
+++ nekurumba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nekurumba
+//
+//  Created by Дмитрий Зубков on 17.07.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
