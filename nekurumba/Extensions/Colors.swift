71641f1aa638625eb2f53801e25a4c765e5f7493
dteethman <zuuuubice@gmail.com>
2020-07-20 15:56:19 +0300

    Add Date extension and some colors to Colors.swift

--- nekurumba/Extensions/Colors.swift
+++ nekurumba/Extensions/Colors.swift
@@ -2,4 +2,6 @@ import UIKit
 
 let bgLightColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
 let bgDarkColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+let greenHighlightColor = UIColor(red: 212/255, green: 242/255, blue: 209/255, alpha: 1)
+let redHighlightColor = UIColor(red: 253/255, green: 172/255, blue: 172/255, alpha: 1)
 let bgColors = (light: bgLightColor, dark: bgDarkColor)
---
+++ nekurumba/Extensions/E_Date.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension Date {
+    static var yesterday: Date { return Date().dayBefore }
+    static var tomorrow:  Date { return Date().dayAfter }
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
+}
