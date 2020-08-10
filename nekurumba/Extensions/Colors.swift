02998110273d13a03a3f9912101e800683d947d5
dteethman <zuuuubice@gmail.com>
2020-08-10 05:36:50 +0300

    Add secondaryLabelColors

--- nekurumba/Extensions/Colors.swift
+++ nekurumba/Extensions/Colors.swift
@@ -6,4 +6,6 @@ let greenHighlightColor = UIColor(red: 212/255, green: 242/255, blue: 209/255, a
 let redHighlightColor = UIColor(red: 253/255, green: 172/255, blue: 172/255, alpha: 1)
 let inactiveColor = UIColor(red: 160/255, green: 160/255, blue: 160/255, alpha: 1)
 let activeColor = UIColor.systemPink
+let secondaryLabelColors = (light: UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6),
+                      dark: UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6))
 let bgColors = (light: bgLightColor, dark: bgDarkColor)
