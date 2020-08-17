52113d843f0b366b8dbdbb5a1de277b02673c317
dteethman <zuuuubice@gmail.com>
2020-08-17 18:35:16 +0300

    Move SliderItem struct to NMMultiLevelCircularSlider.swift

--- nekurumba/Views/NeomorpKit/NMMultiLevelCircularSlider.swift
+++ nekurumba/Views/NeomorpKit/NMMultiLevelCircularSlider.swift
@@ -1,5 +1,14 @@
 import UIKit
 
+struct SliderItem {
+    var sliderValue: Box<CGFloat>
+    var minValue: CGFloat = 0
+    var maxValue: CGFloat = 1
+    var minSliderValue: CGFloat = 0
+    var maxSliderValue: CGFloat = 1
+    var numberOfDivisions: Int = 0
+}
+
 class NMMultiLevelCircularSlider: UIView {
     public var sliderItems: [SliderItem] = []
     private var activeItem: Int!
